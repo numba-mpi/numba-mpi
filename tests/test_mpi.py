b5bd6d4c51dd75b6d26980ce4d0da1d7c258cc45
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-09-18 11:37:49 -0500

    adding pylint workflow, code cleanups, list of MPI links in README

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -1,7 +1,8 @@
-import numba_mpi as mpi
+# pylint: disable=missing-function-docstring,missing-class-docstring,missing-module-docstring
 from mpi4py.MPI import COMM_WORLD
 import numpy as np
 import pytest
+import numba_mpi as mpi
 
 
 class TestMPI:
@@ -23,12 +24,15 @@ class TestMPI:
         assert rank == COMM_WORLD.Get_rank()
 
     @staticmethod
-    @pytest.mark.parametrize("snd, rcv", [(mpi.send, mpi.recv), (mpi.send.py_func, mpi.recv.py_func)])
-    @pytest.mark.parametrize("dt", [np.float64])
-    def test_send_recv(snd, rcv, dt):
-        src = np.array([1, 2, 3, 4, 5], dtype=dt)
-        dst_tst = np.empty(5, dtype=dt)
-        dst_exp = np.empty(5, dtype=dt)
+    @pytest.mark.parametrize("snd, rcv", [
+        (mpi.send, mpi.recv),
+        (mpi.send.py_func, mpi.recv.py_func)
+    ])
+    @pytest.mark.parametrize("data_type", [np.float64])
+    def test_send_recv(snd, rcv, data_type):
+        src = np.array([1, 2, 3, 4, 5], dtype=data_type)
+        dst_tst = np.empty(5, dtype=data_type)
+        dst_exp = np.empty(5, dtype=data_type)
 
         if mpi.rank() == 0:
             snd(src, dest=1, tag=11)
@@ -39,4 +43,3 @@ class TestMPI:
 
             assert np.all(dst_tst == src)
             assert np.all(dst_tst == dst_exp)
-
