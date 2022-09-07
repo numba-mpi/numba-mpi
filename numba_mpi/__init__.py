3f60ffeece7d595e0a3cf448860126c8783764f9
Kacper Derlatka <kacper.derlatka@pega.com>
2022-09-07 20:32:31 +0200

    add package __version__

--- numba_mpi/__init__.py
+++ numba_mpi/__init__.py
@@ -1,2 +1,10 @@
 """ Numba @njittable MPI wrappers tested on Linux, macOS and Windows """
+from pkg_resources import DistributionNotFound, VersionConflict, get_distribution
 from .mpi import initialized, size, rank, send, recv, allreduce, Operator
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except (DistributionNotFound, VersionConflict):
+    # package is not installed
+    pass
