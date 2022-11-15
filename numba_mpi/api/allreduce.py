d09aaa6eece5062d54cc6982296b02addbf68aed
David Zwicker <david.zwicker@ds.mpg.de>
2022-11-15 21:34:03 +0100

    Use `overload` instead of `generated_jit` (#58)

--- numba_mpi/api/allreduce.py
+++ numba_mpi/api/allreduce.py
@@ -2,9 +2,9 @@
 import ctypes
 from numbers import Number
 
-import numba
 import numpy as np
 from numba.core import types
+from numba.extending import overload
 
 from numba_mpi.api.operator import Operator
 from numba_mpi.common import _MPI_Comm_World_ptr, _MpiComm, _MpiDatatype, _MpiOp, libmpi
@@ -22,7 +22,6 @@ _MPI_Allreduce.argtypes = [
 ]
 
 
-@numba.generated_jit(nopython=True)
 def allreduce(
     sendobj, recvobj, operator=Operator.SUM
 ):  # pylint: disable=unused-argument
@@ -30,7 +29,46 @@ def allreduce(
     Note that complex datatypes and user-defined functions are not properly supported.
     Returns integer status code (0 == MPI_SUCCESS)
     """
-    if isinstance(sendobj, (types.Number, Number)):
+    if isinstance(sendobj, Number):
+        # reduce a single number
+        sendobj = np.array([sendobj])
+        status = _MPI_Allreduce(
+            sendobj.ctypes.data,
+            recvobj.ctypes.data,
+            sendobj.size,
+            _mpi_dtype(sendobj),
+            _mpi_addr(operator),
+            _mpi_addr(_MPI_Comm_World_ptr),
+        )
+
+    elif isinstance(sendobj, np.ndarray):
+        # reduce an array
+        sendobj = np.ascontiguousarray(sendobj)
+        status = _MPI_Allreduce(
+            sendobj.ctypes.data,
+            recvobj.ctypes.data,
+            sendobj.size,
+            _mpi_dtype(sendobj),
+            _mpi_addr(operator),
+            _mpi_addr(_MPI_Comm_World_ptr),
+        )
+
+    else:
+        raise TypeError(f"Unsupported type {sendobj.__class__.__name__}")
+
+    return status
+
+
+@overload(allreduce)
+def ol_allreduce(
+    sendobj, recvobj, operator=Operator.SUM
+):  # pylint: disable=unused-argument
+    """wrapper for MPI_Allreduce
+    Note that complex datatypes and user-defined functions are not properly supported.
+    Returns integer status code (0 == MPI_SUCCESS)
+    """
+    if isinstance(sendobj, types.Number):
+        # reduce a single number
 
         def impl(sendobj, recvobj, operator=Operator.SUM):
             sendobj = np.array([sendobj])
@@ -50,7 +88,8 @@ def allreduce(
 
             return status
 
-    elif isinstance(sendobj, (types.Array, np.ndarray)):
+    elif isinstance(sendobj, types.Array):
+        # reduce an array
 
         def impl(sendobj, recvobj, operator=Operator.SUM):
             sendobj = np.ascontiguousarray(sendobj)
