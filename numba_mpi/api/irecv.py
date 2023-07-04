fdf0dfcdd9a9d5917465845cb95982917fddf3ff
Maciej Manna <maciejmanna@gmail.com>
2023-07-04 22:37:33 +0200

    added isend and irecv wrappers

---
+++ numba_mpi/api/irecv.py
@@ -0,0 +1,38 @@
+"""MPI_Irecv() implementation"""
+import ctypes
+
+import numba
+import numpy as np
+from mpi4py.MPI import ANY_SOURCE, ANY_TAG
+
+from numba_mpi.common import _MPI_Comm_World_ptr, libmpi, send_recv_async_args
+from numba_mpi.utils import _mpi_addr, _mpi_dtype
+
+_MPI_Irecv = libmpi.MPI_Irecv
+_MPI_Irecv.restype = ctypes.c_int
+_MPI_Irecv.argtypes = send_recv_async_args
+
+
+@numba.njit()
+def recv(data, source=ANY_SOURCE, tag=ANY_TAG):
+    """Wrapper for MPI_Irecv (only handles contiguous arrays, at least for now).
+    If successful (i.e. result is MPI_SUCCESS), returns c-style pointer to valid
+    MPI_Request handle that may be used with appropriate wait and test functions."""
+
+    assert data.flags.c_contiguous
+
+    request = np.empty(1, np.uintp)
+
+    status = _MPI_Irecv(
+        data.ctypes.data,
+        data.size,
+        _mpi_dtype(data),
+        source,
+        tag,
+        _mpi_addr(_MPI_Comm_World_ptr),
+        request.ctypes.data
+    )
+
+    assert status == 0
+
+    return request[0]
---
+++ numba_mpi/api/isend.py
@@ -0,0 +1,36 @@
+"""MPI_Isend() implementation"""
+import ctypes
+
+import numba
+import numpy as np
+
+from numba_mpi.common import _MPI_Comm_World_ptr, libmpi, send_recv_async_args
+from numba_mpi.utils import _mpi_addr, _mpi_dtype
+
+_MPI_Isend = libmpi.MPI_Isend
+_MPI_Isend.restype = ctypes.c_int
+_MPI_Isend.argtypes = send_recv_async_args
+
+
+@numba.njit
+def isend(data, dest, tag=0):
+    """Wrapper for MPI_Send. If successful (i.e. result is MPI_SUCCESS), 
+        returns c-style pointer to valid MPI_Request handle that may be used
+        with appropriate wait and test functions. Mimicking `mpi4py`, default
+        value for `tag` is set to zero.
+    """
+    data = np.ascontiguousarray(data)
+    request = np.empty(1, np.uintp)
+    status = _MPI_Isend(
+        data.ctypes.data,
+        data.size,
+        _mpi_dtype(data),
+        dest,
+        tag,
+        _mpi_addr(_MPI_Comm_World_ptr),
+        request.ctypes.data
+    )
+
+    assert status == 0
+
+    return request[0]
