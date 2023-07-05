5c065590dbb727c28e220c7b7913e0c7df43a484
Maciej Manna <maciejmanna@gmail.com>
2023-07-05 12:47:24 +0200

    fixed request handling in test; testing - singgular wait full load

--- tests/api/test_isend_irecv.py
+++ tests/api/test_isend_irecv.py
@@ -17,36 +17,6 @@ from tests.utils import get_random_array
         (mpi.isend.py_func, mpi.irecv.py_func, mpi.wait.py_func),
     ),
 )
-def test_isend_irecv(isnd, ircv, wait):
-    src = get_random_array((3, 3))
-    dst_exp = np.empty_like(src)
-    dst_tst = np.empty_like(src)
-
-    if mpi.rank() == 0:
-        req = isnd(src, dest=1, tag=11)
-        req_exp = COMM_WORLD.Isend(src, dest=1, tag=22)
-        wait(req)
-        req_exp.wait()
-    elif mpi.rank() == 1:
-        req = ircv(dst_tst, source=0, tag=11)
-        req_exp = COMM_WORLD.Irecv(dst_exp, source=0, tag=22)
-        wait(req)
-        req_exp.wait()
-
-        np.testing.assert_equal(dst_tst, src)
-        np.testing.assert_equal(dst_exp, src)
-
-
-"""
-
-
-@pytest.mark.parametrize(
-    "isnd, ircv, wait",
-    (
-        (mpi.isend, mpi.irecv, mpi.wait),
-        (mpi.isend.py_func, mpi.irecv.py_func, mpi.wait.py_func),
-    ),
-)
 @pytest.mark.parametrize("data_type", data_types)
 def test_isend_irecv(isnd, ircv, wait, data_type):
     src = get_random_array((3, 3), data_type)
@@ -68,6 +38,7 @@ def test_isend_irecv(isnd, ircv, wait, data_type):
         np.testing.assert_equal(dst_exp, src)
 
 
+"""
 @pytest.mark.parametrize(
     "isnd, ircv, wall, create_reqs",
     [
