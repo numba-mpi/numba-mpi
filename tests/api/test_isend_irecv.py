817b0b34734f66adfb082bf35b85733afc768d3e
Maciej Manna <maciejmanna@gmail.com>
2023-07-05 11:37:02 +0200

    tests - simple, mpi4py only

--- tests/api/test_isend_irecv.py
+++ tests/api/test_isend_irecv.py
@@ -1,7 +1,6 @@
 # pylint: skip-file
 
 # pylint: disable=missing-function-docstring,missing-class-docstring,missing-module-docstring
-"""
 import numpy as np
 import pytest
 from mpi4py.MPI import COMM_WORLD
@@ -9,6 +8,36 @@ from mpi4py.MPI import COMM_WORLD
 import numba_mpi as mpi
 from tests.common import data_types
 from tests.utils import get_random_array
+
+
+@pytest.mark.parametrize(
+    "isnd, ircv, wait",
+    (
+        (mpi.isend, mpi.irecv, mpi.wait),
+        (mpi.isend.py_func, mpi.irecv.py_func, mpi.wait.py_func),
+    ),
+)
+def test_isend_irecv(isnd, ircv, wait):
+    src = get_random_array((3, 3))
+    dst_exp = np.empty_like(src)
+    dst_tst = np.empty_like(src)
+
+    if mpi.rank() == 0:
+        # req = isnd(src, dest=1, tag=11)
+        req_exp = COMM_WORLD.Isend(src, dest=1, tag=22)
+        # wait(req)
+        req_exp.wait()
+    elif mpi.rank() == 1:
+        # req = ircv(dst_tst, source=0, tag=11)
+        req_exp = COMM_WORLD.Irecv(source=0, tag=22)
+        # wait(req)
+        dst_exp = req_exp.wait()
+
+        # np.testing.assert_equal(dst_tst, src)
+        np.testing.assert_equal(dst_exp, src)
+
+
+"""
 
 
 @pytest.mark.parametrize(
