d20af9b860f9b82b84740c4990fc0c8d51510ac0
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-02-07 08:48:28 +0100

    pin pytest version to <8 to avoid module not found issue

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     license="GPL v3",
     description="Numba @njittable MPI wrappers tested on Linux, macOS and Windows",
     install_requires=("numba", "numpy", "mpi4py", "psutil"),
+    extras_require={"tests": ("pytest<8.0.0")},  # TODO #122
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     packages=find_packages(include=["numba_mpi", "numba_mpi.*"]),
