32dd7c4ad5b334794dcf0d6d3aa3d50579dc36df
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2024-02-07 11:33:34 +0100

    add Python 3.12 support (and CI runs) (#111)

--- numba_mpi/__init__.py
+++ numba_mpi/__init__.py
@@ -1,6 +1,6 @@
 """ Numba @njittable MPI wrappers tested on Linux, macOS and Windows """
 
-from pkg_resources import DistributionNotFound, VersionConflict, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 from .api.allreduce import allreduce
 from .api.barrier import barrier
@@ -19,7 +19,7 @@ from .api.wtime import wtime
 from .common import RequestType
 
 try:
-    __version__ = get_distribution(__name__).version
-except (DistributionNotFound, VersionConflict):
+    __version__ = version(__name__)
+except PackageNotFoundError:
     # package is not installed
     pass
