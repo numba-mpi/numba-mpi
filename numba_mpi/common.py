75c3046b64a35ebd42eecff32333838b9a9a7911
sfonxu <wronski.michal4@wp.pl>
2025-03-25 09:41:21 +0100

    add openmpi lib pathfinding for macos to support newer macos versions

--- numba_mpi/common.py
+++ numba_mpi/common.py
@@ -65,11 +65,19 @@ if hasattr(ps, "memory_maps"):
                 if name + ("" if windows else ".") in path.stem:
                     LIB = str(path)
                     break
+
 else:
     for name in names:
         LIB = find_library(name)
         if LIB is not None:
             break
+
+if sys.platform == "darwin" and LIB is None:
+        LIB_bash_str = "$(brew --cellar open-mpi)/$(brew list --versions open-mpi | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"
+        LIB_bash = subprocess.run(f'echo {LIB_bash_str}', shell=True, stdout=subprocess.PIPE)
+        if LIB_bash.returncode == 0:
+            LIB = LIB_bash.stdout.decode('ascii').strip()+"/lib/libmpi.dylib"
+            
 
 if LIB is None:
     raise RuntimeError("no MPI library found")
