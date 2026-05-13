7032070df9aaf1fb23997c35449a2525abd6f2d4
Kamil Kania <kkania388@gmail.com>
2026-05-13 16:06:21 +0200

    Added sendrecv implementation and unit tests

--- numba_mpi/api/sendrecv.py
+++ numba_mpi/api/sendrecv.py
@@ -1,10 +1,72 @@
 """MPI_Sendrecv() implementation"""
 
+import ctypes
+
 import numba
+import numpy as np
+from mpi4py.MPI import ANY_SOURCE, ANY_TAG
+
+from numba_mpi.common import (
+    _MPI_Comm_World_ptr,
+    _MpiComm,
+    _MpiDatatype,
+    _MpiStatusPtr,
+    create_status_buffer,
+    libmpi,
+)
+from numba_mpi.utils import _mpi_addr, _mpi_dtype
+
+_MPI_Sendrecv = libmpi.MPI_Sendrecv
+_MPI_Sendrecv.restype = ctypes.c_int
+
+_MPI_Sendrecv.argtypes = [
+    ctypes.c_void_p,
+    ctypes.c_int,
+    _MpiDatatype,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_void_p,
+    ctypes.c_int,
+    _MpiDatatype,
+    ctypes.c_int,
+    ctypes.c_int,
+    _MpiComm,
+    _MpiStatusPtr,
+]
 
 
 @numba.njit
-def sendrecv():
+def sendrecv(senddata, dest, recvdata, source=ANY_SOURCE, sendtag=0, recvtag=ANY_TAG):
     """
     Placeholder for sendrecv implementation
     """
+
+    senddata = np.ascontiguousarray(senddata)
+
+    status_buffer = create_status_buffer()
+
+    buffer = (
+        recvdata
+        if recvdata.flags.c_contiguous
+        else np.empty(recvdata.shape, recvdata.dtype)
+    )
+
+    status = _MPI_Sendrecv(
+        senddata.ctypes.data,
+        senddata.size,
+        _mpi_dtype(senddata),
+        dest,
+        sendtag,
+        buffer.ctypes.data,
+        buffer.size,
+        _mpi_dtype(recvdata),
+        source,
+        recvtag,
+        _mpi_addr(_MPI_Comm_World_ptr),
+        status_buffer.ctypes.data,
+    )
+
+    if not recvdata.flags.c_contiguous:
+        recvdata[...] = buffer
+
+    return status
--- tests/api/test_sendrecv.py
+++ tests/api/test_sendrecv.py
@@ -1,5 +1,122 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring,missing-module-docstring
 
+import numpy as np
+import pytest
 
-def test_sendrecv():
-    assert False
+import numba_mpi as mpi
+
+
+class TestSendrecv:
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_pairwise_smoke(sr):
+        """Minimal 0↔1 exchange — rank 0’s send tag matches rank 1’s recv tag, and vice versa."""
+        # arrange
+        snd = [
+            np.array([1.0, 2.0], dtype=np.float64),
+            np.array([7.0, 8.0], dtype=np.float64),
+        ]
+        rcv = np.empty_like(snd[0])
+        tags = [11, 22]
+
+        if (r := mpi.rank()) > 1:
+            return
+
+        # act
+        status = sr(snd[r], (r + 1) % 2, rcv, sendtag=tags[r], recvtag=tags[r - 1])
+
+        # assert
+        assert status == mpi.SUCCESS
+        np.testing.assert_array_equal(rcv, snd[r - 1])
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_circular(sr):
+        """Minimal neighbour exchange — rank 0’s send tag matches rank 1’s recv tag, etc"""
+        # arrange
+        send_data = [rank + np.array([1.0, 2.0]) for rank in range(mpi.size())]
+        recv_data = np.empty_like(send_data[0])
+
+        # act
+        status = sr(
+            senddata=send_data[mpi.rank()],
+            dest=(mpi.rank() + 1) % mpi.size(),
+            recvdata=recv_data,
+            sendtag=mpi.rank(),
+            recvtag=(mpi.rank() - 1) % mpi.size(),
+        )
+
+        # assert
+        assert status == mpi.SUCCESS
+        np.testing.assert_array_equal(recv_data, send_data[mpi.rank() - 1])
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_selfsend(sr):
+        # arrange
+        data = np.empt(10)
+
+        # act
+        status = sr(data, mpi.rank(), recvdata=data)
+
+        # assert
+        assert status == mpi.SUCCESS
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_noncontiguous_input(sr):
+        # arrange
+        inpt = np.arange(100).reshape(10, 10)[1:8, 1:8]
+        inpt += 1
+        assert not inpt.flags.c_contiguous
+        oupt = np.zeros_like(inpt)
+        assert oupt.flags.c_contiguous
+
+        # act
+        status = sr(inpt, mpi.rank(), oupt)
+
+        # assert
+        assert status == mpi.SUCCESS
+        assert all(inpt.flatten() == oupt.flatten())
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_noncontiguous_output(sr):
+        # arrange
+        oupt = np.arange(100).reshape(10, 10)[1:8, 1:8]
+        oupt += 1
+        inpt = np.zeros_like(oupt)
+        assert inpt.flags.c_contiguous
+        assert not oupt.flags.c_contiguous
+
+        # act
+        status = sr(inpt, mpi.rank(), oupt)
+
+        # assert
+        assert status == mpi.SUCCESS
+        assert all(inpt.flatten() == oupt.flatten())
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_invalid_dest(sr):
+        # arrange
+        data = np.zeros(10)
+
+        # act
+        status = sr(data, dest=mpi.size(), recvdata=data)
+
+        # assert
+        assert status != mpi.SUCCESS
+
+    @pytest.mark.parametrize("sr", (mpi.sendrecv, mpi.sendrecv.py_func))
+    @staticmethod
+    def test_invalid_source(sr):
+        # arrange
+        data = np.zeros(10)
+
+        # act
+        status = sr(data, 0, source=mpi.size(), recvdata=data)
+
+        # assert
+        assert status != mpi.SUCCESS
