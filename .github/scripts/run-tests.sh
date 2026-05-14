9c48215985e6080b44065e06c8b4520fde78c316
Sylwester Arabas <sylwester.arabas@agh.edu.pl>
2026-05-15 00:35:58 +0200

    Add condition to skip test for OpenMPI v5 on ARM64

--- .github/scripts/run-tests.sh
+++ .github/scripts/run-tests.sh
@@ -17,7 +17,7 @@ export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
 
 pip install --only-binary=:all: -e .[tests,CI_version_pins]
-mpiexec --mca btl_tcp_if_include eth0 -n 2 python -m pytest --durations=10 -p no:unraisableexception -We
+mpiexec --mca btl_tcp_if_include eth0 -n 2 python -m pytest -s -v --durations=10 -p no:unraisableexception -We tests/api
 exit_code=$?
 echo "Tests completed with exit code $exit_code"
 echo $exit_code > /mnt/cluster-workspace/gha-runner/test_exit_code.txt
