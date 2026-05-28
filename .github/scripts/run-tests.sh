efd4d33508c3b55559ce1d66876415fb655bb2ee
Kamil Kania <kkania388@gmail.com>
2026-05-28 12:18:00 +0200

    Bowie CI: Refactor Slurm script and workflow to accept command argument; introduce reusable run-on-bowie action; add common script for README code extraction; run the README code on Bowie and publish plot as action artifact (#218)

--- .github/scripts/run-tests.sh
+++ .github/scripts/run-tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# ###################################################################
-# Slurm script to run tests on Bowie Supercomputer Cluster          #
-# Reference: https://open-atmos-krk.github.io/projects/hpc-diy.html #
-# ###################################################################
+# #######################################################################
+# Slurm script to run jobs on Bowie Supercomputer Cluster               #
+# Info on Bowie: https://open-atmos-krk.github.io/projects/hpc-diy.html #
+# Expected arguments:                                                   # 
+#   1: Python version (passed to pyenv)                                 #
+#   2: command to execute (passed to mpiexec)                           #
+# #######################################################################
 
 #SBATCH --output=/mnt/cluster-workspace/gha-runner/logs/stdout.txt
 #SBATCH --error=/mnt/cluster-workspace/gha-runner/logs/joberr.txt
@@ -17,7 +20,7 @@ export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
 
 pip install --only-binary=:all: -e .[tests,CI_version_pins]
-mpiexec --mca btl_tcp_if_include eth0 -n 2 python -m pytest -s -v --durations=10 -p no:unraisableexception -We tests/api
+mpiexec --mca btl_tcp_if_include eth0 -n 2 $2
 exit_code=$?
 echo "Tests completed with exit code $exit_code"
 echo $exit_code > /mnt/cluster-workspace/gha-runner/test_exit_code.txt
